5ab466c5183dd054e4f9f2ac0a6635f79f10845e
Skywalkingzulu <114562400+Skywalkingzulu1@users.noreply.github.com>
2023-09-14 13:43:13 +0200

    Update script.js

--- public/script.js
+++ public/script.js
@@ -2,41 +2,60 @@ const chatDiv = document.getElementById("chat");
 const userInput = document.getElementById("userInput");
 const sendButton = document.getElementById("sendButton");
 let conversationHistory = []; // Initialize conversation history
-
-// Initialize Netlify Identity
-const netlifyIdentity = window.netlifyIdentity;
-netlifyIdentity.init();
-
-// Login event listener
-netlifyIdentity.on('login', (user) => {
-    // Handle user login here
-    console.log('User logged in:', user);
-});
-
-// Logout event listener
-netlifyIdentity.on('logout', () => {
-    // Handle user logout here
-    console.log('User logged out');
-});
+let currentUsername = "";
 
 sendButton.addEventListener("click", async () => {
     const userMessage = userInput.value;
     if (userMessage.trim() !== "") {
         // Append user message to the conversation history with "role" set to "user"
-        conversationHistory.push({ role: "user", message: userMessage });
+        conversationHistory.push({ role: "user", message: userMessage }); // Do not include the username here
         userInput.value = "";
 
         // Call the chatbot API with the current conversation history
         const chatbotResponse = await getChatbotResponse(userMessage, conversationHistory);
 
         // Append chatbot response to the conversation history with "role" set to "assistant"
-        conversationHistory.push({ role: "assistant", message: chatbotResponse });
+        conversationHistory.push({ role: "assistant", message: chatbotResponse }); // Do not include the username here
+
+        // Save the conversation to restdb.io, linking the username to each message
+        saveConversationToRestDB(conversationHistory);
 
         // Update the UI to display the conversation
         updateUI();
     }
 });
 
+// Function to save the conversation history to restdb.io
+function saveConversationToRestDB(history) {
+    const apiKey = "265a3cb410e1b1ace0303ec60467a38932fe2"; // Replace with your actual restdb.io API key
+    const apiUrl = "https://nkanyezi-2737.restdb.io/rest/conversation"; // Adjust the URL as needed
+
+    // Get the username from the cookie
+    const username = getCookie("username");
+
+    // Add the username to each message in the conversation history
+    const historyWithUsername = history.map(message => ({
+        ...message,
+        username,
+    }));
+
+    // Send the conversation data to restdb.io using an AJAX POST request
+    fetch(apiUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'x-apikey': apiKey,
+        },
+        body: JSON.stringify(historyWithUsername), // Send the entire conversation history with usernames
+    })
+    .then(response => response.json())
+    .then(newDocument => {
+        console.log('New document created:', newDocument);
+    })
+    .catch(error => {
+        console.error('Error creating document:', error);
+    });
+}
 function appendMessage(sender, message) {
     const messageContainer = document.createElement("div"); // Create a container div
 
@@ -66,7 +85,7 @@ async function getChatbotResponse(message, history) {
         chatbot_global_action: "Act as an assistant",
         previous_history: history, // Pass the entire conversation history
         temperature: 0.0,
-        max_tokens: 150,
+        max_tokens: 650,
     };
 
     try {
@@ -99,3 +118,23 @@ function updateUI() {
         appendMessage(role, message);
     });
 }
+
+// Your existing code...
+
+// Add an event listener for the "Paste from Document" button
+const pasteButton = document.getElementById("pasteButton");
+
+pasteButton.addEventListener("click", async () => {
+    try {
+        // Read the clipboard contents
+        const clipboardText = await navigator.clipboard.readText();
+
+        // Set the clipboard text as the user's message and trigger a chatbot response
+        userInput.value = clipboardText;
+
+        // Simulate a click on the "Send" button to send the message
+        sendButton.click();
+    } catch (error) {
+        console.error("Error reading clipboard:", error);
+    }
+});
